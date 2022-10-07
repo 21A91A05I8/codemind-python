da9858da559842f8290a1c2872181086f48b9e01
21A91A05I8 <97424594+21A91A05I8@users.noreply.github.com>
2022-10-07 18:33:24 +0530

    n=int(input()) a=0 b=1 c=a+b f=0 if(n==0 or n==1):     print("True") for i in range(1,n+1,1):     c=a+b     a=b     b=c     if(c==n):         f=1         print("True")         break     else:         c=c+1 if(f==0):     print("False")

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,16 +1,19 @@
-def is_fib_number(num):
-    a=0
-    b=1
-    while b<num:
-        c=a+b
-        a=b
-        b=c
-    if b==num or a==num:
-        return True
-    if b>num:
-        return False
-num=int(input())
-if is_fib_number(num):
-    print(True)
-else:
+n=int(input())
+a=0
+b=1
+c=a+b
+f=0
+if(n==0 or n==1):
+    print("True")
+for i in range(1,n+1,1):
+    c=a+b
+    a=b
+    b=c
+    if(c==n):
+        f=1
+        print("True")
+        break
+    else:
+        c=c+1
+if(f==0):
     print("False")
