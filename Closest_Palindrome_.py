7854cf13fd4db4e950dbb7e3bc29a99ef387e137
21A91A05I8 <97424594+21A91A05I8@users.noreply.github.com>
2022-10-07 18:37:40 +0530

    def pal(n):     temp=n     s=0     while(n!=0):         d=n%10         s=s*10+d         n=n//10     if(s==temp):         return 1     else:         return 0 n=int(input()) for i in range(1,n):     if(pal(i)):         k=i i=n+1 while(1):     if(pal(i)):         m=i         break     i=i+1 if(abs(n-k)>abs(n-m)):     print(m) elif(abs(n-k)==abs(n-m)):     print(k,m) else:     print(k)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,32 +1,27 @@
-def palin(n):
-    rev=0
-    p=n
-    while(n):
-        r=n%10
+def pal(n):
+    temp=n
+    s=0
+    while(n!=0):
+        d=n%10
+        s=s*10+d
         n=n//10
-        rev=rev*10+r
-    if rev==p:
-        return p
-n=int(input()) 
-c1=0
-c2=0
+    if(s==temp):
+        return 1
+    else:
+        return 0
+n=int(input())
+for i in range(1,n):
+    if(pal(i)):
+        k=i
 i=n+1
-k=n-1
-while True:
-    c1=c1+1
-    if palin(i)==i:
-        z=i
+while(1):
+    if(pal(i)):
+        m=i
         break
     i=i+1
-while True:
-    c2=c2+1
-    if palin(k)==k:
-        p=k
-        break
-    k=k-1
-if c1==c2:
-    print(p,z)
-elif c1<c2:
-    print(z)
+if(abs(n-k)>abs(n-m)):
+    print(m)
+elif(abs(n-k)==abs(n-m)):
+    print(k,m)
 else:
-    print(p)
+    print(k)
